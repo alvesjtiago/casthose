cc682f11154e051914a9a17842274c5bfdf87f3a
Tiago Alves <alvesjtiago@gmail.com>
2023-09-07 23:20:15 +0100

    Add footer and remove logs

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
 
   const getStream = async () => {
     const nodeClient = getHubRpcClient(
-      'https://622393.hubs-web.neynar.com:2285',
+      process.env.NEXT_PUBLIC_HUB_URL ?? '',
       {},
     )
 
@@ -34,9 +34,9 @@ export default function Home() {
     result.map((observable) => {
       observable.subscribe({
         async next(event: HubEvent) {
-          console.log('received event', event)
           const message =
             event?.mergeMessageBody?.message?.data?.castAddBody?.text
+
           if (message) {
             const userResponse = await (
               await fetch(
@@ -64,7 +64,7 @@ export default function Home() {
           console.error(err)
         },
         complete() {
-          console.log('Observable completed now')
+          console.log('Observable completed')
         },
       })
     })
@@ -75,36 +75,40 @@ export default function Home() {
   }, [])
 
   return (
-    <div className="container mx-auto max-w-2xl my-12 px-8">
-      {casts.length == 0 && <div>Waiting for first cast...</div>}
-      {casts?.map((cast) => {
-        return (
-          <div className="mt-5 flex" key={cast.hash?.toString()}>
-            <a
-              href={`http://warpcast.com/${cast.username}`}
-              target="_blank"
-              className="flex-shrink-0"
-            >
-              <img
-                className="h-6 w-6 mr-4 flex-shrink-0 rounded-full object-cover"
-                src={cast.avatarUrl}
-                alt={cast.username}
-              />
-            </a>
-            <div>
+    <div className="pb-24 overflow-auto h-screen">
+      <div className="container mx-auto max-w-2xl my-12 px-8">
+        {casts.length == 0 && <div>Waiting for first cast...</div>}
+        {casts?.map((cast) => {
+          return (
+            <div className="mt-5 flex" key={cast.hash?.toString()}>
               <a
-                href={`http://warpcast.com/${cast.username}/${bytesToHexString(
-                  cast?.hash as Uint8Array,
-                )._unsafeUnwrap()}`}
+                href={`http://warpcast.com/${cast.username}`}
                 target="_blank"
-                className="text-ellipsis overflow-hidden whitespace-pre-line [overflow-wrap:anywhere]"
+                className="flex-shrink-0"
               >
-                {cast.text}
+                <img
+                  className="h-6 w-6 mr-4 flex-shrink-0 rounded-full object-cover"
+                  src={cast.avatarUrl}
+                  alt={cast.username}
+                />
               </a>
+              <div>
+                <a
+                  href={`http://warpcast.com/${
+                    cast.username
+                  }/${bytesToHexString(
+                    cast?.hash as Uint8Array,
+                  )._unsafeUnwrap()}`}
+                  target="_blank"
+                  className="text-ellipsis overflow-hidden whitespace-pre-line [overflow-wrap:anywhere]"
+                >
+                  {cast.text}
+                </a>
+              </div>
             </div>
-          </div>
-        )
-      })}
+          )
+        })}
+      </div>
     </div>
   )
 }
